6540d4ebaf2ce65eff341f91ff35ac6986ce23d2
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-03-16 15:50:23 +0900

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# change directory to the shell file's directory
+SCRIPT_DIR=`dirname $0`
+cd $SCRIPT_DIR
+
+# symbolic link
+# ln -s .bashrc ~/.bashrc
+# ln -s .vimrc ~/.vimrc
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    echo link "$f"
+    ln -s "$f" ~/"$f"
+done
