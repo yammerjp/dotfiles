122843969a0a8e7ac33abbedd07f2833bf43f31f
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-06 17:23:09 +0900

    Add yarn-init.sh

---
+++ bin/yarn-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# change directory to the shell file's directory
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+cd "$SCRIPT_DIR"
+
+ech(){ sh "$SCRIPT_DIR/echo.sh" "$*"; }
+
+which yarn && :
+if [ "$?" != "0" ];then
+  ech "Need yarn. Prease install yarn and retry the script."
+  exit 1
+fi
+
+yarn global add
