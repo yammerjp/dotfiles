71c4a0970f7b45da422261bd57d49f2af735761c
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-08 01:39:24 +0900

    Add apt

--- bin/packages-init.sh
+++ bin/packages-init.sh
@@ -6,6 +6,7 @@ cd "$DOTFILES_DIR"
 ech(){ sh "$DOTFILES_DIR/bin/echo.sh" "$*"; }
 
 if [ "$(uname)" = "Darwin" ];then
+
   ech "Install packages for macOS"
   if ! which brew > /dev/null 2>&1 ; then
     ech "Need homebrew. Prease install homebrew and retry the script."
@@ -18,5 +19,27 @@ if [ "$(uname)" = "Darwin" ];then
  fi
  brew bundle --file "$DOTFILES_DIR/etc/brewfile"
 
+elif [ "$(uname)" = "Linux" ];then
+
+  ech "Install packages for Linux"
+  if [ "`whoami`" != "root" ]; then
+    echo "Require root privilege"
+    exit 1
+  fi
+  if ! which apt > /dev/null 2>&1 ; then
+    ech "Need apt"
+    exit 1
+  fi
+
+  curl -sL https://deb.nodesource.com/setup_13.x | bash -
+  apt update
+  apt upgrade -y
+
+  cat "$DOTFILES_DIR/etc/aptfile" | while read -r PACKAGE
+  do
+    apt-get install -y "$PACKAGE"
+  done
+
+  exit 0
 fi
 
