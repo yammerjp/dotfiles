23ac74216f1a86355d852c3c797027df8007c578
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-23 14:09:37 +0900

    CommentOut sourcekit-lsp

--- bin/packages-init/common.sh
+++ bin/packages-init/common.sh
@@ -6,12 +6,12 @@ cd "$DOTFILES_DIR"
 ech(){ sh "$DOTFILES_DIR/bin/echo.sh" "$*"; }
 
 # sourcekit-lsp
-if ! which sourcekit-lsp > /dev/null 2>&1 ; then
-  ech "Install sourcekit-lsp (lsp for swift)"
-  git clone https://github.com/apple/sourcekit-lsp.git /tmp/sourcekit-lsp
-  cd /tmp/sourcekit-lsp
-  swift build -c release
-  mv .build/release/sourcekit-lsp /usr/local/bin
-  ech "Finished sourcekit-lsp"
-fi
+#if ! which sourcekit-lsp > /dev/null 2>&1 ; then
+#  ech "Install sourcekit-lsp (lsp for swift)"
+#  git clone https://github.com/apple/sourcekit-lsp.git /tmp/sourcekit-lsp
+#  cd /tmp/sourcekit-lsp
+#  swift build -c release
+#  mv .build/release/sourcekit-lsp /usr/local/bin
+#  ech "Finished sourcekit-lsp"
+#fi
 
