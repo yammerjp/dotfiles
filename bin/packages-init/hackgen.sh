602778c298eccf4b6af2ce361a233c1eaea2f4a6
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-28 23:54:47 +0900

    Fix to install HackGen for Ubuntu

---
+++ bin/packages-init/hackgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+if [ "$(whoami)" != "root" ]; then
+  echo "Require root privilege"
+  exit 1
+fi
+
+TMP_FILE="/tmp/dotfiles/hackgen"
+TMP_FILE_ZIP="$TMP_FILE.zip"
+TMP_DIR="$(dirname "$TMP_FILE_ZIP")"
+
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+cd "$TMP_DIR"
+
+VERSION="$(curl -sI https://github.com/yuru7/HackGen/releases/latest | grep location | awk -F '/' '{print $NF }')"
+# 改行削除
+VERSION=`echo ${VERSION} | sed -e "s/[\r\n]\+//g"`
+URL="https://github.com/yuru7/HackGen/releases/download/$VERSION/HackGen_$VERSION.zip"
+
+echo "Download HackGen font from $URL"
+curl -fsSL "$URL" -o "$TMP_FILE_ZIP"
+unzip "$TMP_FILE"
+mv "HackGen_$VERSION" "/usr/share/fonts/HackGen"
+fc-cache -fv
+
--- bin/packages-init/ubuntu.sh
+++ bin/packages-init/ubuntu.sh
@@ -60,12 +60,6 @@ snap install chromium
 snap install docker
 
 # ========== HackGen ==========
-TMP_FILE="/tmp/dotfiles/hackgen.zip"
-mkdir -p "$(dirname "$TMP_FILE")"
-VERSION="$(curl -sI https://github.com/yuru7/HackGen/releases/latest | grep location | awk -F '/' '{print $NF }')"
-mkdir -p "$(dirname "$TMP_HYPER")"
-curl "https://github.com/yuru7/HackGen/releases/download/$VERSION/HackGen_$VERSION.zip" -o "$TMP_FILE"
-unzip "$TMP_FILE"
-mv "HackGen_$VERSION" "/usr/share/fonts/HackGen"
-fc-cache -fv
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+bash "$SCRIPT_DIR/hackgen.sh"
 
