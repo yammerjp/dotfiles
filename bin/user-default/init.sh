b926ccaf60fdac9bcad4f7fcb44d821b4663df0d
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-04-29 19:07:55 +0900

    Skip shiftit.sh

--- bin/user-default/init.sh
+++ bin/user-default/init.sh
@@ -9,5 +9,6 @@ echo  "Set User Default with 'defaults' command"
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 
 bash "$SCRIPT_DIR/os.sh"
-bash "$SCRIPT_DIR/shiftit.sh"
+# Please execute ~/dotfiles/bin/user-default/shiftit.sh after first booting shiftit.
+# bash "$SCRIPT_DIR/shiftit.sh"
 
