123f054f105b8db43b3004171e9bb595d8e32063
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-02 19:32:56 +0900

    Refactoring to be more readable

--- bin/ubuntu-packages.sh
+++ bin/ubuntu-packages.sh
@@ -1,4 +1,82 @@
 #!/bin/bash -e
+# ======================================== Function Definition ========================================
+Install_nodejs () {
+  curl -sL https://deb.nodesource.com/setup
+  apt update
+  apt upgrade -y
+  apt install -y nodejs
+}
+
+Install_git () {
+  add-apt-repository -y ppa:git-core/ppa
+  apt install -y git
+}
+
+Install_yarn () {
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+  YARN_PACKAGE_URL='deb https://dl.yarnpkg.com/debian/ stable main'
+  YARN_LIST='/etc/apt/sources.list.d/yarn.list'
+  if ! grep -q "$YARN_PACKAGE_URL" "$YARN_LIST" ; then
+    echo "$YARN_PACKAGE_URL" >> "$YARN_LIST"
+  fi
+  apt update
+  apt install -y yarn
+}
+
+Install_hackgen () {
+  TMP_FILE="/tmp/dotfiles/hackgen"
+  TMP_FILE_ZIP="$TMP_FILE.zip"
+  TMP_DIR="$(dirname "$TMP_FILE_ZIP")"
+
+  rm -rf "$TMP_DIR"
+  mkdir -p "$TMP_DIR"
+  cd "$TMP_DIR"
+
+  VERSION="$(curl -sI https://github.com/yuru7/HackGen/releases/latest | grep location | awk -F '/' '{print $NF }')"
+  # 改行削除
+  # shellcheck disable=SC2001
+  VERSION="${VERSION//[\r\n]\+/}"
+  URL="https://github.com/yuru7/HackGen/releases/download/$VERSION/HackGen_$VERSION.zip"
+
+  echo "Download HackGen font from $URL"
+  curl -fsSL "$URL" -o "$TMP_FILE_ZIP"
+  unzip "$TMP_FILE"
+  mv "HackGen_$VERSION" "/usr/share/fonts/HackGen"
+  fc-cache -fv
+}
+
+Install_vscode () {
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+  sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  apt install -y apt-transport-https
+  apt update
+  apt install -y code # or code-insiders
+  # https://wiki.debian.org/VisualStudioCode
+  # https://code.visualstudio.com/docs/setup/linux
+}
+
+Install_spotify () {
+  curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | apt-key add -
+  SPOTIFY_PACKAGE_URL="deb http://repository.spotify.com stable non-free"
+  SPOTIFY_LIST="/etc/apt/sources.list.d/spotify.list"
+  if ! grep -q "$SPOTIFY_PACKAGE_URL" "$SPOTIFY_LIST" ; then
+    echo "$SPOTIFY_PACKAGE_URL" >> "$SPOTIFY_LIST"
+  fi
+  apt update
+  apt install -y spotify-client
+  # https://www.spotify.com/jp/download/linux/
+}
+
+Install_vivaldi () {
+  wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | apt-key add -
+  add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
+  apt update
+  apt install -y vivaldi-stable
+  #  https://help.vivaldi.com/ja/article/manual-setup-vivaldi-linux-repositories/
+}
+
+# ======================================== Package Installation ========================================
 
 echo "Install packages"
 
@@ -14,33 +92,12 @@ fi
 echo "Install packages for CUI"
 
 apt install -y curl
-
-# ==================== nodejs ====================
-curl -sL https://deb.nodesource.com/setup
-apt update
-apt upgrade -y
-apt install -y nodejs
-
-# ==================== git ====================
-add-apt-repository -y ppa:git-core/ppa
-apt install -y git
-
-# ==================== yarn ====================
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-YARN_PACKAGE_URL='deb https://dl.yarnpkg.com/debian/ stable main'
-YARN_LIST='/etc/apt/sources.list.d/yarn.list'
-if ! grep -q "$YARN_PACKAGE_URL" "$YARN_LIST" ; then
-  echo "$YARN_PACKAGE_URL" >> "$YARN_LIST"
-fi
-
-apt update
-apt install -y yarn
-
-# ==================== apt packages ====================
+Install_nodejs
+Install_git
+Install_yarn
 apt install -y vim
 apt install -y zsh
 apt install -y tmux
-
 apt install -y colordiff
 apt install -y gdebi
 apt install -y ffmpeg
@@ -49,8 +106,6 @@ apt install -y nmap
 apt install -y shellcheck
 apt install -y tree
 apt install -y imagemagick
-
-# ==================== snap packages ====================
 snap install --classic heroku
 snap install docker
 
@@ -61,66 +116,14 @@ fi
 
 echo "Install packages for GUI"
 
-# ==================== HackGen ====================
-TMP_FILE="/tmp/dotfiles/hackgen"
-TMP_FILE_ZIP="$TMP_FILE.zip"
-TMP_DIR="$(dirname "$TMP_FILE_ZIP")"
-
-rm -rf "$TMP_DIR"
-mkdir -p "$TMP_DIR"
-cd "$TMP_DIR"
-
-VERSION="$(curl -sI https://github.com/yuru7/HackGen/releases/latest | grep location | awk -F '/' '{print $NF }')"
-# 改行削除
-# shellcheck disable=SC2001
-VERSION="${VERSION//[\r\n]\+/}"
-URL="https://github.com/yuru7/HackGen/releases/download/$VERSION/HackGen_$VERSION.zip"
-
-echo "Download HackGen font from $URL"
-curl -fsSL "$URL" -o "$TMP_FILE_ZIP"
-unzip "$TMP_FILE"
-mv "HackGen_$VERSION" "/usr/share/fonts/HackGen"
-fc-cache -fv
-
-# ==================== apt packages ====================
+Install_hackgen
+Install_vscode
+Install_spotify
+Install_vivaldi
 apt install -y ibus-mozc
 apt install -y gimp
 apt install -y screenruler
-
-# ==================== snap packages ====================
 snap install chromium
 snap install pick-colour-picker
 # snap install --classic shotcut
-
-# ==================== VScode ====================
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt install -y apt-transport-https
-apt update
-apt install -y code # or code-insiders
-# https://wiki.debian.org/VisualStudioCode
-# https://code.visualstudio.com/docs/setup/linux
-
-# ==================== slack ====================
-#snap install --classic slack
-echo "Please install slack from deb package downloaded by web-browser !!"
-
-# ==================== spotify ====================
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | apt-key add -
-SPOTIFY_PACKAGE_URL="deb http://repository.spotify.com stable non-free"
-SPOTIFY_LIST="/etc/apt/sources.list.d/spotify.list"
-if ! grep -q "$SPOTIFY_PACKAGE_URL" "$SPOTIFY_LIST" ; then
-  echo "$SPOTIFY_PACKAGE_URL" >> "$SPOTIFY_LIST"
-fi
-apt update
-apt install -y spotify-client
-# https://www.spotify.com/jp/download/linux/
-
-# ==================== vivaldi ====================
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | apt-key add -
-add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
-apt update
-apt install -y vivaldi-stable
-#  https://help.vivaldi.com/ja/article/manual-setup-vivaldi-linux-repositories/
-
+echo "Please install slack from deb package downloaded by web-browser !!" # snap install --classic slack
