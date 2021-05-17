fdbd229b973449f263d9a004106710d40ba1acbc
Keisuke Nakayama <me@yammer.jp>
2021-05-18 08:57:25 +0900

    Add a shell-script to install private dotfiles

---
+++ bin/download-install-private.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+DOTDIR="$HOME/dev/github.com/yammerjp/dotfiles-private"
+git clone https://github.com/yammerjp/dotfiles-private.git $DOTDIR
+cd $DOTDIR
+
+mkdir -p bin
+curl -sL https://raw.githubusercontent.com/yammerjp/dotfiles/bin/link.sh > ./bin/link.sh
+bash bin/link.sh
+
+# ssh directory permission settings
