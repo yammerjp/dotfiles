4d2065b9c47da4ffd61d88c2757f2f39a42f6de7
Keisuke Nakayama <me@yammer.jp>
2021-06-01 19:39:34 +0900

    Update: install-private script

--- bin/download-install-private.sh
+++ bin/download-install-private.sh
@@ -4,8 +4,5 @@ DOTDIR="$HOME/src/github.com/yammerjp/dotfiles-private"
 git clone https://github.com/yammerjp/dotfiles-private.git $DOTDIR
 cd $DOTDIR
 
-mkdir -p bin
-curl -sL https://raw.githubusercontent.com/yammerjp/dotfiles/bin/link.sh > ./bin/link.sh
 bash bin/link.sh
-
 # ssh directory permission settings
