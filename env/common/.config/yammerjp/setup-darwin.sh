e9874dd28a115cc24849e4686fd8eb54b81f0a5c
Keisuke Nakayama <me@yammer.jp>
2022-04-16 07:43:24 +0000

    Fix lint

--- env/common/.config/yammerjp/setup-darwin.sh
+++ env/common/.config/yammerjp/setup-darwin.sh
@@ -7,11 +7,11 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 
 # os initializing setup
-bash ~/.config/yammerjp/darwin-userdefaults.sh
+bash "$HOME/.config/yammerjp/darwin-userdefaults.sh"
 
 # package install
 ARCH="$(uname -m)" # arm64 or x86_64
-brew bundle --file "~/.config/homebrew/package-list-darwin-${ARCH}"
+brew bundle --file "$HOME/.config/homebrew/package-list-darwin-${ARCH}"
 
 eval "rbenv init -"
 latest_stable_ruby="$(rbenv install -l 2>/dev/null | grep -e '^[0-9. ]\+$' | tail -1)"
--- env/common/.setup.sh
+++ env/common/.setup.sh
@@ -4,9 +4,11 @@ OS="$(uname -s)"          # Darwin Linux
 DIST="$(os_distribution)" # Ubuntu         # allow empty
 
 if [ "$OS" = Darwin ]; then
-  source ~/.config/yammerjp/setup-darwin.sh
+  # shellcheck disable=SC1090
+  source "$HOME/.config/yammerjp/setup-darwin.sh"
 elif [ "$OS" = Linux ] && [ "$DIST" = Ubuntu ]; then
-  source ~/.config/yammerjp/setup-linux-ubuntu.sh
+  # shellcheck disable=SC1090
+  source "$HOME/.config/yammerjp/setup-linux-ubuntu.sh"
 else
-  echo "setup script is not found..." 1&>2
+  echo "setup script is not found..."
 fi
