22fb9f74885f8a52a03e8eedb742a391c5cdd5bf
Keisuke Nakayama <me@yammer.jp>
2022-10-03 15:11:56 +0900

    fix lint

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # for GitHub codespaces
 
-SCRIPT_DIR=`dirname $0`
-DOTFILE_DIRS="$SCRIPT_DIR" ${SCRIPT_DIR}/.bin/dotfiles link
-DOTFILE_DIRS="$SCRIPT_DIR" ${SCRIPT_DIR}/.bin/dotfiles setup
+SCRIPT_DIR=$(dirname "$0")
+DOTFILE_DIRS="$SCRIPT_DIR" "${SCRIPT_DIR}/.bin/dotfiles" link
+DOTFILE_DIRS="$SCRIPT_DIR" "${SCRIPT_DIR}/.bin/dotfiles" setup
