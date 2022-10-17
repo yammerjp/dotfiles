5b515beeb6e835105de8dbbb8c6062da169cd9e7
Keisuke Nakayama <me@yammer.jp>
2022-10-17 13:12:56 +0900

    add the command `git-pull-latest-all`

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -8,16 +8,22 @@ function gclone() {
     echo 'Usage: gclone username reponame # clone git repository from github.com'
     return 1
   fi
-  if [[ "$1" =~ '/' ]]; then
-    repo="$1"
+  if [[ "$1" =~ '\.' ]]; then
+    host="$1"
+    repo="$2/$3"
   else
-    repo="$1/$2"
+    host="github.com"
+    if [[ "$1" =~ '/' ]]; then
+      repo="$1"
+    else
+      repo="$1/$2"
+    fi
+    if [ -z "$2" ]; then
+      repo="yammerjp/$1"
+    fi
   fi
-  if [ -z "$2" ]; then
-    repo="yammerjp/$1"
-  fi
-  clone_to="$HOME/src/github.com/${repo}"
-  clone_from="git@github.com:${repo}.git"
+  clone_to="$HOME/src/${host}/${repo}"
+  clone_from="git@${host}:${repo}.git"
   if ! git clone "${clone_from}" "${clone_to}"; then
     echo "Failed to clone from '${clone_from}' to '${clone_to}'"
     return 1
@@ -30,10 +36,50 @@ function gclone() {
 function ghq_code() {
   local repo="$(ghq list | fzf --preview "bat --color=always --style=numbers --line-range=:100 $(ghq root)/{}/README.*")"
   if [ -n "$repo" ]; then
-    BUFFER="code $(ghq root)/${repo} && exit"
+    BUFFER="cd $(ghq root)/${repo}"
     zle accept-line
   fi
   zle clear-screen
 }
 zle -N ghq_code
 bindkey '^f' ghq_code
+
+function cr() {
+  cd "$(ghq root)/$(ghq list | fzf)"
+}
+
+function git-pull-latest() {
+  if ! git -C "$GIT_REPOSITORY_PATH" diff --exit-code -s; then
+    echo "\e[31mfailed to pull, found diff: $GIT_REPOSITORY_PATH\e[m" 1>&2
+    echo "$GIT_REPOSITORY_PATH" >> ~/.git-pull-latest.git-repository-path.found-diff.log
+    return
+  fi
+
+  git -C "$GIT_REPOSITORY_PATH" fetch origin
+
+  local head="$(git -C "$GIT_REPOSITORY_PATH" rev-parse --abbrev-ref HEAD)"
+  if [ "$head" != "master" ] && [ "$head" != "main" ]; then
+    git -C "$GIT_REPOSITORY_PATH" checkout master || git -C "$GIT_REPOSITORY_PATH" checkout main
+  fi
+
+  head="$(git -C "$GIT_REPOSITORY_PATH" rev-parse --abbrev-ref HEAD)"
+  if [ "$head" != "master" ] && [ "$head" != "main" ]; then
+    echo "\e[31mfailed to checkout to master: $GIT_REPOSITORY_PATH\e[m" 1>&2
+    echo "$GIT_REPOSITORY_PATH" >> ~/.git-pull-latest.git-repository-path.failed-to-checkout.log
+    return
+  fi
+
+  git -C "$GIT_REPOSITORY_PATH" merge origin/"$head" --ff-only
+}
+
+function git-pull-latest-all() {
+  rm  ~/.git-pull-latest.git-repository-path.*.log
+  ghq list | awk "{printf \"$(ghq root)/%s\n\", \$1}" | while read GIT_REPOSITORY_PATH; do
+  echo "git-pull-latest: $GIT_REPOSITORY_PATH" 1>&2
+    git-pull-latest
+  done
+}
+
+function cd-failed-directory-git-pull-latest-all() {
+  cd $(cat ~/.git-pull-latest.*.log | fzf)
+}
