99146912bd3aadb2e0c81eee1f920392cd7f81c0
Keisuke Nakayama <me@yammer.jp>
2022-10-20 08:26:39 +0900

    fix ci

--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -e
 # for GitHub codespaces
 
-SCRIPT_DIR=$(dirname "$0")
-DOTFILE_DIRS="$SCRIPT_DIR" "${SCRIPT_DIR}/.local/bin/dotfiles" link
-DOTFILE_DIRS="$SCRIPT_DIR" "${SCRIPT_DIR}/.local/bin/dotfiles" setup
+if ! which git > /dev/null 2>&1; then
+  echo "Need git" 1>&2
+  exit
+fi
+
+if [ "$DOTFILES_DIR" == "" ]; then
+  HOME_DOTFILES="$HOME/dotfiles"
+  if git -C "$HOME_DOTFILES" ls-files > /dev/null 2>&1 ; then
+    DOTFILES_DIR="$HOME_DOTFILES"
+  fi
+  DOTFILES_DIR="https://github.com/yammerjp/dotfiles"
+fi
+
+# setup yadm
+curl -fLo ~/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ~/yadm
+~yadm list > /dev/null 2>&1 | ~/yadm clone "$DOTFILES_REPO"
+~/yadm clone
+~/yadm bootstrap
+
+# setup packages
+bash ~/.setup.sh
+
