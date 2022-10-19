93ef8446f1ada177b8a1874bd62e50e38d781ca2
Keisuke Nakayama <me@yammer.jp>
2022-10-20 08:49:42 +0900

    chmod +x for boot strap on GitHub Actions

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ fi
 # setup yadm
 curl -fLo ~/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x ~/yadm
 ~/yadm list > /dev/null 2>&1 || ~/yadm clone "$DOTFILES_REPO"
+chmod a+x ~/.config/yadm/bootstrap
 ~/yadm bootstrap
 
 # setup packages
