9ecf8971f78f4fd8159dbe3bb77defedc134ffca
Keisuke Nakayama <me@yammer.jp>
2022-11-07 15:44:01 +0000

    support yadm

--- .config/zsh/detect-to-edit-dotfiles.zsh
+++ .config/zsh/detect-to-edit-dotfiles.zsh
@@ -1,5 +1,4 @@
-DOTFILES_DIR="$HOME/src/github.com/yammerjp/dotfiles"
-if ! git --work-tree=$DOTFILES_DIR --git-dir=$DOTFILES_DIR/.git diff --exit-code > /dev/null ; then
+if ! yadm diff --exit-code > /dev/null ; then
   echo 'Please commit and push dotfiles diff!!!'
-  git --work-tree=$DOTFILES_DIR --git-dir=$DOTFILES_DIR/.git diff --name-only | cat
+  yadm diff --name-only | cat
 fi
