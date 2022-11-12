80447a85c0f764ee6e78ce20767671b2b663652e
Keisuke Nakayama <me@yammer.jp>
2022-11-12 08:04:12 +0000

    rm duplicated loading rust development tools

--- .config/zsh/darwin--arm64.zsh
+++ .config/zsh/darwin--arm64.zsh
@@ -5,12 +5,6 @@ fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
 # x86_64版のbrewを実行するとき, =brew で実行させる
 setopt magic_equal_subst
 
-# for Rust lang
-if [ -e "$HOME/.cargo/env" ]; then
-  source "$HOME/.cargo/env"
-  alias cr='cargo run'
-fi
-
 if [ -e "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk" ]; then
   source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
   source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
--- .config/zsh/linux-ubuntu.zsh
+++ .config/zsh/linux-ubuntu.zsh
@@ -3,12 +3,6 @@ alias pst="xclip -selection clipboard -o"
 alias ls='ls -Fh --color=auto'
 alias open='xdg-open'
 
-# for Rust lang
-if [ -e "$HOME/.cargo/env" ]; then
-  source "$HOME/.cargo/env"
-  alias cr='cargo run'
-fi
-
 if [ -e "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
