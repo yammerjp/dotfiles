7fc8e9b3ac5d83301cf37ecf5feae5642dabdad3
Keisuke Nakayama <me@yammer.jp>
2022-11-20 12:03:00 +0900

    fix setup.sh

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
-help:
-	@cat Makefile
-lint:
+.PHONY: help, lint, docker/build, docker/run
+# https://ktrysmt.github.io/blog/write-useful-help-command-by-shell/
+help: ## print this message ## make help
+	@echo "Example operations by makefile."
+	@echo ""
+	@echo "Usage: make SUB_COMMAND argument_name=argument_value"
+	@echo ""
+	@echo "Command list:"
+	@echo ""
+	@printf "\033[36m%-30s\033[0m %-50s %s\n" "[Sub command]" "[Description]" "[Example]"
+	@grep -E '^[/a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | perl -pe 's%^([/a-zA-Z_-]+):.*?(##)%$$1 $$2%' | awk -F " *?## *?" '{printf "\033[36m%-30s\033[0m %-50s %s\n", $$1, $$2, $$3}'
+lint: ## lint shell scripts ## make lint
 	@find . -type f | grep -E "\.sh$$|\.bin/dotfiles$$" | xargs shellcheck
+docker/build: ## build docker image ## make docker/build
+	docker build . -t yammerjp/dotfiles:latest
+docker/run: ## run docker image ## make docker/run
+	docker run -it -v $$(pwd):/home/yammer/dotfiles -e LOCAL_UID=$$(id -u $$USER) -e LOCAL_GID=$$(id -g $$USER) yammerjp/dotfiles bash
--- setup.sh
+++ setup.sh
@@ -6,26 +6,29 @@ if ! command -v git > /dev/null 2>&1; then
   exit
 fi
 
+# find dotfiles repository
 if [ "$DOTFILES_REPO" == "" ]; then
   HOME_DOTFILES="$HOME/dotfiles/.git"
   if git -C "$HOME_DOTFILES" ls-files > /dev/null 2>&1 ; then
+    # CI and Docker
     DOTFILES_REPO="$HOME_DOTFILES"
+  else
+    # other
+    DOTFILES_REPO="https://github.com/yammerjp/dotfiles"
   fi
-  DOTFILES_REPO="https://github.com/yammerjp/dotfiles"
+  echo "dotfiles-repo: $DOTFILES_REPO"
 fi
 
+# setup yadm
 mkdir -p "$HOME/.local/bin"
 YADM_PROGRAM="$HOME/.local/bin/yadm"
+curl -fLo "$YADM_PROGRAM" https://github.com/TheLocehiliosan/yadm/raw/master/yadm \
+  && chmod a+x "$YADM_PROGRAM"
 
-# setup yadm
-curl -fLo "$YADM_PROGRAM" https://github.com/TheLocehiliosan/yadm/raw/master/yadm && chmod a+x "$YADM_PROGRAM"
-echo "dotfiles-repo: $DOTFILES_REPO"
-"$YADM_PROGRAM" list > /dev/null 2>&1 || "$YADM_PROGRAM" clone "$DOTFILES_REPO" --bootstrap
-ls -al "$HOME"
-echo "checkout all files"
-"$YADM_PROGRAM" checkout "$HOME"
-chmod a+x "$HOME/.config/yadm/bootstrap"
-"$YADM_PROGRAM" bootstrap
+# clone dotfiles repository
+# shellcheck disable=SC2097,SC2098
+"$YADM_PROGRAM" list -a > /dev/null 2>&1 \
+  || YADM_PROGRAM="$YADM_PROGRAM" "$YADM_PROGRAM" clone "$DOTFILES_REPO" --bootstrap
 
 # setup packages
 bash ~/.setup.sh
