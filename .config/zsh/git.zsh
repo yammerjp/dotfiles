056c840a6b99298a626173b427c47807f92f8c3c
Keisuke Nakayama <me@yammer.jp>
2022-12-30 12:45:39 +0900

    rm git-pull-latest*

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -57,39 +57,3 @@ function cr() {
   fi
   cd "$(ghq root)/$repo_path_from_root"
 }
-
-function git-pull-latest() {
-  if ! git -C "$GIT_REPOSITORY_PATH" diff --exit-code -s; then
-    echo "\e[31mfailed to pull, found diff: $GIT_REPOSITORY_PATH\e[m" 1>&2
-    echo "$GIT_REPOSITORY_PATH" >> ~/.git-pull-latest.git-repository-path.found-diff.log
-    return
-  fi
-
-  git -C "$GIT_REPOSITORY_PATH" fetch origin
-
-  local head="$(git -C "$GIT_REPOSITORY_PATH" rev-parse --abbrev-ref HEAD)"
-  if [ "$head" != "master" ] && [ "$head" != "main" ]; then
-    git -C "$GIT_REPOSITORY_PATH" checkout master || git -C "$GIT_REPOSITORY_PATH" checkout main
-  fi
-
-  head="$(git -C "$GIT_REPOSITORY_PATH" rev-parse --abbrev-ref HEAD)"
-  if [ "$head" != "master" ] && [ "$head" != "main" ]; then
-    echo "\e[31mfailed to checkout to master: $GIT_REPOSITORY_PATH\e[m" 1>&2
-    echo "$GIT_REPOSITORY_PATH" >> ~/.git-pull-latest.git-repository-path.failed-to-checkout.log
-    return
-  fi
-
-  git -C "$GIT_REPOSITORY_PATH" merge origin/"$head" --ff-only
-}
-
-function git-pull-latest-all() {
-  rm  ~/.git-pull-latest.git-repository-path.*.log
-  ghq list | awk "{printf \"$(ghq root)/%s\n\", \$1}" | while read GIT_REPOSITORY_PATH; do
-  echo "git-pull-latest: $GIT_REPOSITORY_PATH" 1>&2
-    git-pull-latest
-  done
-}
-
-function cd-failed-directory-git-pull-latest-all() {
-  cd $(cat ~/.git-pull-latest.*.log | fzf)
-}
