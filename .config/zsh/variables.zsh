dea6d16347b89480b1bd097d52c2a9080c2ba2b2
Keisuke Nakayama <me@yammer.jp>
2023-01-22 22:35:00 +0900

    Fix path directory order

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -5,17 +5,17 @@ export XDG_STATE_HOME="$HOME/.local/state"
 
 # (N-/) ... ディレクトリが存在するときのみ追加
 path=(
-  $HOME/bin(N-/)
-  $HOME/.bin(N-/)
-  $HOME/.local/bin(N-/)
-  $HOME/.config/npm-packages/bin(N-/)
-  $HOME/go/bin(N-/)
-  $HOME/.cargo/bin(N-/)
-  $HOME/.composer/vendor/bin(N-/)
+  $path
   /usr/local/bin(N-/)
   /usr/local/sbin(N-/)
   /opt/homebrew/bin(N-/)
   /opt/homebrew/sbin(N-/)
   /opt/homebrew/opt/luajit-openresty/bin(N-/)
-  $path
+  $HOME/go/bin(N-/)
+  $HOME/.cargo/bin(N-/)
+  $HOME/.composer/vendor/bin(N-/)
+  $HOME/.config/npm-packages/bin(N-/)
+  $HOME/bin(N-/)
+  $HOME/.bin(N-/)
+  $HOME/.local/bin(N-/)
 )
