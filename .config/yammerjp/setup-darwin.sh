4f487a782d036b046d27508ddfa008e785db2c7d
Keisuke Nakayama <me@yammer.jp>
2023-02-05 08:57:08 +0900

    Remove rbenv, ... and install package from Brewfile.(hostname)

--- .config/yammerjp/setup-darwin.sh
+++ .config/yammerjp/setup-darwin.sh
@@ -1,35 +1,16 @@
 #!/bin/bash -e
 
-# download ... dotfiles/bin/download.sh
-# link     ... make link
-
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "$SCRIPT_DIR"
 
 # os initializing setup
 bash "$HOME/.config/yammerjp/darwin-userdefaults.sh"
 
-# package install
-ARCH="$(uname -m)" # arm64 or x86_64
-brew bundle --file "$HOME/.config/homebrew/package-list-darwin-${ARCH}"
-
-eval "rbenv init -"
-latest_stable_ruby="$(rbenv install -l 2>/dev/null | grep -e '^[0-9. ]\+$' | tail -1)"
-
-rbenv install "$latest_stable_ruby"
-rbenv global "$latest_stable_ruby"
-
-eval "nodenv init -"
-latest_stable_node="$(nodenv install -l 2>/dev/null | grep -e '^[0-9. ]\+$' | tail -1)"
-nodenv install "$latest_stable_node"
-nodenv global "$latest_stable_node"
-
-eval "pyenv init -"
-latest_stable_python="$(pyenv install -l 2>/dev/null | grep -e '^[0-9. ]\+$' | tail -1)"
-pyenv install "$latest_stable_python"
-pyenv global "$latest_stable_python"
-
-# nvim plugin install
-nvim -s "$HOME/.config/nvim/plugin-install.vim"
+BREWFILE="$XDG_CONFIG_HOME/homebrew/Brewfile.$(hostname)"
+if [ -e "$BREWFILE" ]; then
+  brew bundle --file "$BREWFILE"
+else
+  printf "Brewfile is not found ...\n%s\n" "$BREWFILE" 1>&2
+fi
 
 echo "Nothing to do" 1>&2
