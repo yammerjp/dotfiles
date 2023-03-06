eadadd9220f60238adf0056a1a00b61274caec52
Keisuke Nakayama <me@yammer.jp>
2023-03-06 20:14:50 +0900

    Update to judgement platforms for installing packages

--- setup.sh
+++ setup.sh
@@ -24,21 +24,16 @@ if [ "$CODE_SPACES" == "true" ]; then
 fi
 
 #### SETUP PACKAGES
-function os_distribution() {
-  if uname -v | grep -q "Ubuntu"; then  # 0 ... ubuntu / 1 ... other
-    echo 'Ubuntu'
-    return
-  fi
-}
-
-OS="$(uname -s)"          # Darwin Linux
-DIST="$(os_distribution)" # Ubuntu         # allow empty
-if [ "$OS" = Darwin ]; then
-  # shellcheck disable=SC1090
-  "$HOME/setup-darwin.sh"
-elif [ "$OS" = Linux ] && [ "$DIST" = Ubuntu ]; then
-  # shellcheck disable=SC1090
-  "$HOME/setup-ubuntu.sh"
-else
-  echo "setup script is not found..."
-fi
+case "$(uname -v)" in
+  Darwin*)
+    # shellcheck disable=SC1090
+    "$HOME/setup-darwin.sh"
+    ;;
+  *Ubuntu*)
+    # shellcheck disable=SC1090
+    "$HOME/setup-ubuntu.sh"
+    ;;
+  *)
+    echo "setup script is not found..."
+    ;;
+esac
