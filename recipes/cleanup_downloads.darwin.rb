ec80d6f5b9baf9ea3ae9a38f527d1e026275d6b0
Keisuke Nakayama <me@yammer.jp>
2025-04-09 22:00:16 +0900

    add launchctl scheduler to cleanup ~/Downloads

---
+++ recipes/cleanup_downloads.darwin.rb
@@ -0,0 +1,58 @@
+# スクリプトのパス（ホームディレクトリ取得）
+user = ENV['USER']
+home = "/Users/#{user}"
+script_path = "#{home}/bin/cleanup_downloads.sh"
+plist_path = "#{home}/Library/LaunchAgents/com.#{user}.cleanupdownloads.plist"
+
+# スクリプト本体
+directory "#{home}/bin"
+
+file script_path do
+  owner user
+  mode "755"
+  content <<~EOS
+    #!/bin/bash
+    find "#{home}/Downloads" -type f -mtime +7 -delete
+  EOS
+end
+
+# launchd plist
+file plist_path do
+  owner user
+  mode "644"
+  content <<~XML
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>com.#{user}.cleanupdownloads</string>
+
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{script_path}</string>
+        </array>
+
+        <key>StartCalendarInterval</key>
+        <dict>
+          <key>Hour</key>
+          <integer>3</integer>
+          <key>Minute</key>
+          <integer>0</integer>
+        </dict>
+
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>/tmp/cleanup_downloads.err</string>
+        <key>StandardOutPath</key>
+        <string>/tmp/cleanup_downloads.out</string>
+      </dict>
+    </plist>
+  XML
+end
+
+# launchctl load（初回読み込みだけ）
+execute "launchctl load #{plist_path}" do
+  not_if "launchctl list | grep com.#{user}.cleanupdownloads"
+end
