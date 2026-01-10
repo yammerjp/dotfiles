8cd386c228c9fb135450b96e22029adb0878e79a
Keisuke Nakayama <me@yammer.jp>
2026-01-10 19:46:50 +0900

    ~/.zshrcにはツールが追記する時があるので、dotfilesでtrackしない

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -1,3 +1,40 @@
+# # please load this file from ~/.zshr
+# if [ -e ~/.config/zsh/init.zsh ]; then
+#   source ~/.config/zsh/init.zsh
+# fi
+
+zstyle ':completion:*' rehash true
+autoload -Uz compaudit
+autoload -Uz compinit
+compinit -C
+
+function main () {
+  ZSH_SCRIPT_PATHS=( \
+    $HOME/.zshrc.company \
+    $HOME/.zshrc.private \
+    $HOME/.zshrc.local \
+  )
+  for ZSH_SCRIPT_PATH in $ZSH_SCRIPT_PATHS; do
+    if [ -e $ZSH_SCRIPT_PATH ]; then
+      source "$ZSH_SCRIPT_PATH"
+    fi
+  done
+}
+
+main
+
+# zsh initial time measurement
+if (which zprof > /dev/null) ;then
+  zprof | cat
+fi
+
+if (which mise > /dev/null); then
+  alias rtx=mise
+  eval "$(mise activate)"
+fi
+
+alias ztime="time (ZSH_TIME=true zsh -i -c exit)"
+
 function os_distribution() {
   if uname -v | grep -q "Ubuntu"; then  # 0 ... ubuntu / 1 ... other 
     echo 'Ubuntu'
@@ -19,7 +56,6 @@ ZSH_SCRIPT_FILENAMES=( \
   "alias.zsh" \
   "git.zsh" \
   "homebrew.zsh" \
-#  "gdb.zsh" \
   "gitw.zsh" \
   "detect-to-edit-dotfiles.zsh" \
   "completion.zsh" \
@@ -30,14 +66,6 @@ for ELEMENT in $ZSH_SCRIPT_FILENAMES; do
     source "$HOME/.config/zsh/$ELEMENT"
   fi
 done
-
-# ---
-# TODO
-# commitしていないファイルがあるかを確認
-# fetchの最終日から時間が経っているかを確認 (毎回fetchしてられない)
-# pushの最終日から時間が経っているかを確認 (毎回pushしてられない)
-# それらを確認して最新に更新し、更新日を記録する関数を追加
-
 
 # # 計測
 # # for i in $(seq 1 10); do time zsh -i -c exit; done
